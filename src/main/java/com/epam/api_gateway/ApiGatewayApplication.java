26b6fab92781111cb9e1e0545dfd0e1815a87b9d
RealBoy <nallabothulanagarjuna0@gmail.com>
2025-01-26 13:30:04 +0530

    Removed eureka registration

--- src/main/java/com/epam/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/epam/api_gateway/ApiGatewayApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
